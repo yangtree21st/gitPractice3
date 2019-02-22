a81fb254eef0824a8f405b8d90705f473dd98d1a
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-02-22 18:06:43 -0500

    completed tests, implemented methods, and wrote documentation for Dice Class

--- src/main/java/io/zipcoder/casino/Models/Dice.java
+++ src/main/java/io/zipcoder/casino/Models/Dice.java
@@ -1,12 +1,25 @@
 package io.zipcoder.casino.Models;
 
 import java.util.Random;
-
+/**
+ * The Dice program simply simulates a pair of dice being rolled.
+ *
+ * @version 2019-02-22
+ *
+ */
 public class Dice {
     Random random;
 
-    public Long getDiceRoll(){
-        return null;
+    /**
+     * This method is get the dice roll sum of the dice rolled.
+     * @return Integer This returns the sum of both dice rolled.
+     */
+    public Integer getDiceRoll(){
+        this.random = new Random();
+        Integer dieOne = random.nextInt((6)+1);
+        Integer dieTwo = random.nextInt((6)+1);
+        Integer roll = dieOne + dieTwo;
+        return roll;
     }
 
 
