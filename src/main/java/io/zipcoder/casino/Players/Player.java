9997c9b7f03a1d9ef3b3bb9630f68b505cbae5de
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-23 12:31:07 -0500

    Added display class, and updated players with getter and setters

--- src/main/java/io/zipcoder/casino/Players/Player.java
+++ src/main/java/io/zipcoder/casino/Players/Player.java
@@ -3,7 +3,21 @@ package io.zipcoder.casino.Players;
 import io.zipcoder.casino.Guest;
 
 public abstract class Player{
+
+
     private Guest guest;
+
+    public Player(Guest guest){
+        this.guest = guest;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
 
     public void addFunds(Double amount){
         guest.addFunds(amount);
