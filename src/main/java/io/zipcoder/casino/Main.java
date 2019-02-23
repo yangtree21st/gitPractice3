1633591d4f41fee2dd5b48131e67d2769fbe34ab
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-23 13:16:55 -0500

    Updates to main function

--- src/main/java/io/zipcoder/casino/Main.java
+++ src/main/java/io/zipcoder/casino/Main.java
@@ -1,10 +1,21 @@
 package io.zipcoder.casino;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
         Casino casino = new Casino();
         casino.startCasinoExperience();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(createContent()));
+        stage.show();
     }
 
 }
