8d47f4f9e9fda258999218fb1c8e2aaedebe7270
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-24 12:38:20 -0500

    Added display classes

--- src/main/java/io/zipcoder/casino/Main.java
+++ src/main/java/io/zipcoder/casino/Main.java
@@ -1,21 +1,32 @@
 package io.zipcoder.casino;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application{
+    public static Display startingDisplay = new Display();
+    public static Stage mainStage;
+    public static Casino casino = new Casino();
 
     public static void main(String[] args) {
-        Casino casino = new Casino();
-        casino.startCasinoExperience();
-        launch(args);
+        String choice = consoleOrGUI();
+        if(choice.equals("console")) {
+            casino.startCasinoExperience();
+        }else{
+            launch(args);
+        }
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createContent()));
-        stage.show();
+        this.mainStage = stage;
+        this.mainStage.setScene(new Scene(startingDisplay.createContent()));
+        this.mainStage.show();
     }
 
+    public static String consoleOrGUI(){
+        return (casino.console.getStringInput("Would you like to gamble using the <Console> or <GUI> Interface").toLowerCase());
+    }
 }
