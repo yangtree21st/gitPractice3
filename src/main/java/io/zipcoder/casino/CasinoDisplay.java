162d054830c4eb305add885a787a5930248fee60
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-25 16:22:38 -0500

    Remove GridPaneExample

--- src/main/java/io/zipcoder/casino/CasinoDisplay.java
+++ src/main/java/io/zipcoder/casino/CasinoDisplay.java
@@ -14,31 +14,48 @@ import javafx.stage.Stage;
 
 public class CasinoDisplay extends Display {
 
-    //Casino casino = new Casino();
+    Text casinoBanner = new Text("We hope you enjoy your experience!");
+
+    Text casinoSubBanner = new Text("Please enter the below information to create a guest account");
+
+    Text enterNameField = new Text("Enter your full name");
+    TextField nameField = new TextField();
+
+    Text enterStartingBalanceField = new Text("Enter starting balance");
+    TextField startingBalanceField = new TextField();
 
 
     public Parent createCasinoContent() {
-        GridPane casinoGrid = super.createGrid();
+        GridPane casinoGrid = createStandardGridPane();
 
-        TextArea areaInfo = new TextArea();
-        areaInfo.setPrefRowCount(2);
-
-        Text casinoBanner = new Text("We hope you enjoy your experience!");
         //casinoBanner.setTextAlignment(TextAlignment.CENTER);
         casinoBanner.setFont(Font.font ("Verdana", 35));
 
-        Text casinoSubBanner = new Text("Please enter the below information to create a guest account");
         //casinoSubBanner.setTextAlignment(TextAlignment.CENTER);
         casinoSubBanner.setFont(Font.font ("Verdana", 15));
 
-        Text enterNameField = new Text("Enter your full name");
-        TextField nameField = new TextField();
-
-        Text enterStartingBalanceField = new Text("Enter starting balance");
-        TextField startingBalanceField = new TextField();
-
         Button submitInfo = new Button("Submit Information");
         submitInfo.setMaxWidth(Double.MAX_VALUE);
+
+        casinoGrid.add(submitInfo, 2, 4,1,1);
+
+
+        submitInfo.setOnAction(e -> {
+            Main.mainStage.setScene(new Scene(createSecondaryCasinoContent()));
+            Main.mainStage.show();
+        });
+
+
+
+        return casinoGrid;
+    }
+
+    public Parent createSecondaryCasinoContent(){
+
+        GridPane casinoGrid = createStandardGridPane();
+
+        TextArea areaInfo = new TextArea();
+        areaInfo.setPrefRowCount(2);
 
         Button btnEnterCasinoFloor = new Button("Enter Game Floor");
         btnEnterCasinoFloor.setMaxWidth(Double.MAX_VALUE);
@@ -51,30 +68,21 @@ public class CasinoDisplay extends Display {
         Button btnExit = new Button("Exit Casino ");
         btnExit.setMaxWidth(Double.MAX_VALUE);
 
+        //getUserInput
+        String name = nameField.getText();
+        Double amount = Double.parseDouble(startingBalanceField.getText());
 
-        casinoGrid.add(casinoBanner, 0,0, 5,1);
-        casinoGrid.add(casinoSubBanner, 1,1, 3,1);
-        casinoGrid.add(enterNameField, 1,2,1,1);
-        casinoGrid.add(nameField, 1,3,1,1);
-        casinoGrid.add(enterStartingBalanceField, 3,2,1,1);
-        casinoGrid.add(startingBalanceField, 3,3,1,1);
-        casinoGrid.add(submitInfo, 2, 4,1,1);
+        areaInfo.setText(Main.casino.initializeAccountCreation(name, amount));
 
 
-        submitInfo.setOnAction(e -> {
-            String name = nameField.getText();
-            Double amount = Double.parseDouble(startingBalanceField.getText());
-            casinoGrid.add(areaInfo, 1,4,3,1);
-            areaInfo.setText(Main.casino.initializeAccountCreation(name, amount));
-            nameField.setEditable(false);
-            startingBalanceField.setEditable(false);
-            areaInfo.setEditable(false);
-            casinoGrid.getChildren().remove(submitInfo);
-            casinoGrid.add(btnCashier, 1, 5,1,1);
-            casinoGrid.add(btnEnterCasinoFloor, 2, 5,1,1);
-            casinoGrid.add(btnExit, 3, 5,1,1);
+        nameField.setEditable(false);
+        startingBalanceField.setEditable(false);
+        areaInfo.setEditable(false);
 
-        });
+        casinoGrid.add(areaInfo, 1,4,3,1);
+        casinoGrid.add(btnCashier, 1, 5,1,1);
+        casinoGrid.add(btnEnterCasinoFloor, 2, 5,1,1);
+        casinoGrid.add(btnExit, 3, 5,1,1);
 
         btnExit.setOnAction(e -> {
             Stage casinoStage = super.setExitAction(btnExit);
@@ -95,6 +103,19 @@ public class CasinoDisplay extends Display {
         });
 
         return casinoGrid;
+    }
+
+    public GridPane createStandardGridPane(){
+        GridPane standardGridPane = super.createGrid();
+
+        standardGridPane.add(casinoBanner, 0,0, 5,1);
+        standardGridPane.add(casinoSubBanner, 1,1, 3,1);
+        standardGridPane.add(enterNameField, 1,2,1,1);
+        standardGridPane.add(nameField, 1,3,1,1);
+        standardGridPane.add(enterStartingBalanceField, 3,2,1,1);
+        standardGridPane.add(startingBalanceField, 3,3,1,1);
+
+        return standardGridPane;
     }
 
 }
