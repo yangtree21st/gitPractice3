3241d11002c1297443ab204a7b62ae7cb985dd11
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-25 12:32:48 -0500

    undid a change to the game interface

--- src/main/java/io/zipcoder/casino/Interfaces/Game.java
+++ src/main/java/io/zipcoder/casino/Interfaces/Game.java
@@ -2,6 +2,21 @@ package io.zipcoder.casino.Interfaces;
 
 public interface Game {
 
+    public void updateDisplay();
+
+    public void setUp();
+
+    public void takeTurn();
+
+    public void quitGame();
+
+    public void winning(
+
+    );
+
+    public void losing();
+
     public void playFullGame();
+
 
 }
