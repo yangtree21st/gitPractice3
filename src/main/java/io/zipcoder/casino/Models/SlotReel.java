5f4827d34867778c2e080aed3a76b1a66b12f39f
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-26 11:37:28 -0500

    Created slot reel class, created AnsiStuff class, edited DiceGame and Card

--- src/main/java/io/zipcoder/casino/Models/SlotReel.java
+++ src/main/java/io/zipcoder/casino/Models/SlotReel.java
@@ -1,9 +1,11 @@
 package io.zipcoder.casino.Models;
 
+import io.zipcoder.casino.Casino;
+
 import java.util.Random;
 
 public class SlotReel {
-    static final String[][] numbers = {
+    static final String[][] fancyAsciiNumbers = {
             {"  ______  ",
                     " /      \\ ",
                     "/$$$$$$  |",
@@ -14,15 +16,15 @@ public class SlotReel {
                     "$$   $$$/ ",
                     " $$$$$$/  "},
 
-            {"   __   ",
-                    " _/  |  ",
-                    "/ $$ |  ",
-                    "$$$$ |  ",
-                    "  $$ |  ",
-                    "  $$ |  ",
-                    " _$$ |_ ",
-                    "/ $$   |",
-                    "$$$$$$/ "},
+            {"    __    ",
+                    "  _/  |   ",
+                    " / $$ |   ",
+                    " $$$$ |   ",
+                    "   $$ |   ",
+                    "   $$ |   ",
+                    "  _$$ |_  ",
+                    " / $$   | ",
+                    " $$$$$$/  "},
 
             {"  ______  ",
                     " /      \\ ",
@@ -106,7 +108,7 @@ public class SlotReel {
 
     };
 
-    Random randomReel;
+    private Random randomReel;
 
     /**
      * Default constructor, simply initializes the random.
@@ -117,11 +119,22 @@ public class SlotReel {
 
     /**
      * For testing purposes, to use a rigged slotReel to check the different scenarios.
-     *
      * @param testRandom the Random provided with a known seed.
      */
     SlotReel(Random testRandom) {
         randomReel = testRandom;
+    }
+
+    /**
+     * This method gets the results of the slot reel spins and dipslays the result. It also returns the slot reel
+     * results as an int array.
+     * @param numOfReels the number of reels for this slot machine.
+     * @return an int array that contains the results of the middle line of the slot machine spin.
+     */
+    public int[] getSlotReelResultsAndDisplayRepresentation(int numOfReels) {
+        int[] slotReelResults = getSlotReelResults(numOfReels);
+        Casino.console.println(getStringRepresentationOfSlotReelResults(slotReelResults));
+        return slotReelResults;
     }
 
     /**
@@ -141,27 +154,30 @@ public class SlotReel {
     /**
      * This method creates a string representation of the results of slot reels.
      *
-     * @return
+     * @return the string representation of slot machine results.
      */
     public String getStringRepresentationOfSlotReelResults(int[] slotReelResults) {
         StringBuilder representation = new StringBuilder();
 
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < slotReelResults.length; j++) {
-                representation.append(numbers[slotReelResults[j]][i]).append(" | ");
+        for (int k = -1; k <= 1; k++) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < slotReelResults.length; j++) {
+                    representation.append("|               ").append(fancyAsciiNumbers[Math.floorMod(slotReelResults[j] + k, 10)][i]).append("               |");
+                }
+                representation.append('\n');
             }
-            representation.append('\n');
         }
-
 
         return representation.toString();
     }
 
+
+
+
     public static void main(String[] args) {
         SlotReel testReel = new SlotReel();
-        int[] asdf = {5, 8, 2};
-        System.out.println(testReel.getStringRepresentationOfSlotReelResults(asdf));
+        int[] results = {0, 4, 9};
+        System.out.println(testReel.getStringRepresentationOfSlotReelResults(results));
     }
 
 }
