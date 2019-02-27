669f9d706111ca4ed7346a490bf33754e563584e
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-02-27 00:22:37 -0500

    Completed Image util tests

--- src/test/java/io/zipcoder/casino/UtilitiesTest/ImageUtilitiesTest.java
+++ src/test/java/io/zipcoder/casino/UtilitiesTest/ImageUtilitiesTest.java
@@ -1,6 +1,8 @@
 package io.zipcoder.casino.UtilitiesTest;
 
 import io.zipcoder.casino.utilities.ImageUtilities;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,14 +12,17 @@ import java.util.TreeMap;
 public class ImageUtilitiesTest {
 
     ImageUtilities imageUtilities;
+    ImageView imageView;
 
     @Before
     public void generateImageUtilities() {
         imageUtilities = new ImageUtilities();
+        imageView = new ImageView();
+
     }
 
     @Test
-    public void constructorTestSize() {
+    public void constructorTestSizeTest() {
         //given
         Integer expected = 52;
         //when
@@ -27,7 +32,7 @@ public class ImageUtilitiesTest {
     }
 
     @Test
-    public void setTreeMapSmall(){
+    public void setTreeMapSmallTest(){
         //given
         TreeMap<String, String> expectedTreeMap = new TreeMap<>();
         //when
@@ -55,65 +60,48 @@ public class ImageUtilitiesTest {
     }
 
     @Test
-    public void setTreeMap() {
+    public void getHeartAceFromTreeMapTest(){
         //given
-        TreeMap<String, String> cardImageTreeMap = new TreeMap<>();
-        //CARD PATH INITIALIZATION
-        String constantPath = "File:src/main/java/io/zipcoder/casino/Images/";
-        cardImageTreeMap.put("HEARTSACE", constantPath + "AH.png");
-        cardImageTreeMap.put("HEARTSTWO", constantPath + "2H.png");
-        cardImageTreeMap.put("HEARTSTHREE", constantPath + "3H.png");
-        cardImageTreeMap.put("HEARTSFOUR", constantPath + "4H.png");
-        cardImageTreeMap.put("HEARTSFIVE", constantPath + "5H.png");
-        cardImageTreeMap.put("HEARTSSIX", constantPath + "6H.png");
-        cardImageTreeMap.put("HEARTSSEVEN", constantPath + "7H.png");
-        cardImageTreeMap.put("HEARTSEIGHT", constantPath + "8H.png");
-        cardImageTreeMap.put("HEARTSNINE", constantPath + "9H.png");
-        cardImageTreeMap.put("HEARTSTEN", constantPath + "10H.png");
-        cardImageTreeMap.put("HEARTSJACK", constantPath + "JH.png");
-        cardImageTreeMap.put("HEARTSQUEEN", constantPath + "QH.png");
-        cardImageTreeMap.put("HEARTSKING", constantPath + "KH.png");
-        cardImageTreeMap.put("SPADESACE", constantPath + "AS.png");
-        cardImageTreeMap.put("SPADESTWO", constantPath + "2S.png");
-        cardImageTreeMap.put("SPADESTHREE", constantPath + "3S.png");
-        cardImageTreeMap.put("SPADESFOUR", constantPath + "4S.png");
-        cardImageTreeMap.put("SPADESFIVE", constantPath + "5S.png");
-        cardImageTreeMap.put("SPADESSIX", constantPath + "6S.png");
-        cardImageTreeMap.put("SPADESSEVEN", constantPath + "7S.png");
-        cardImageTreeMap.put("SPADESEIGHT", constantPath + "8S.png");
-        cardImageTreeMap.put("SPADESNINE", constantPath + "9S.png");
-        cardImageTreeMap.put("SPADESTEN", constantPath + "10S.png");
-        cardImageTreeMap.put("SPADESJACK", constantPath + "JS.png");
-        cardImageTreeMap.put("SPADESQUEEN", constantPath + "QS.png");
-        cardImageTreeMap.put("SPADESKING", constantPath + "KS.png");
-        cardImageTreeMap.put("DIAMONDSACE", constantPath + "AD.png");
-        cardImageTreeMap.put("DIAMONDSTWO", constantPath + "2D.png");
-        cardImageTreeMap.put("DIAMONDSTHREE", constantPath + "3D.png");
-        cardImageTreeMap.put("DIAMONDSFOUR", constantPath + "4D.png");
-        cardImageTreeMap.put("DIAMONDSFIVE", constantPath + "5D.png");
-        cardImageTreeMap.put("DIAMONDSSIX", constantPath + "6D.png");
-        cardImageTreeMap.put("DIAMONDSSEVEN", constantPath + "7D.png");
-        cardImageTreeMap.put("DIAMONDSEIGHT", constantPath + "8D.png");
-        cardImageTreeMap.put("DIAMONDSNINE", constantPath + "9D.png");
-        cardImageTreeMap.put("DIAMONDSTEN", constantPath + "10D.png");
-        cardImageTreeMap.put("DIAMONDSJACK", constantPath + "JD.png");
-        cardImageTreeMap.put("DIAMONDSQUEEN", constantPath + "QD.png");
-        cardImageTreeMap.put("DIAMONDSKING", constantPath + "KD.png");
-        cardImageTreeMap.put("CLUBSACE", constantPath + "AC.png");
-        cardImageTreeMap.put("CLUBSTWO", constantPath + "2C.png");
-        cardImageTreeMap.put("CLUBSTHREE", constantPath + "3C.png");
-        cardImageTreeMap.put("CLUBSFOUR", constantPath + "4C.png");
-        cardImageTreeMap.put("CLUBSFIVE", constantPath + "5C.png");
-        cardImageTreeMap.put("CLUBSSIX", constantPath + "6C.png");
-        cardImageTreeMap.put("CLUBSSEVEN", constantPath + "7C.png");
-        cardImageTreeMap.put("CLUBSEIGHT", constantPath + "8C.png");
-        cardImageTreeMap.put("CLUBSNINE", constantPath + "9C.png");
-        cardImageTreeMap.put("CLUBSTEN", constantPath + "10C.png");
-        cardImageTreeMap.put("CLUBSJACK", constantPath + "JC.png");
-        cardImageTreeMap.put("CLUBSQUEEN", constantPath + "QC.png");
-        cardImageTreeMap.put("CLUBSKING", constantPath + "KC.png");
-
+        String expectedPath = "File:src/main/java/io/zipcoder/casino/Images/AH.png";
+        //when
+        String actualPath = imageUtilities.getCardImageTreeMap().get("HEARTSACE");
+        //then
+        Assert.assertEquals(expectedPath,actualPath);
     }
 
+    @Test
+    public void getSpadesKingFromTreeMapTest(){
+        //given
+        String expectedPath = "File:src/main/java/io/zipcoder/casino/Images/KS.png";
+        //when
+        String actualPath = imageUtilities.getCardImageTreeMap().get("SPADESKING");
+        //then
+        Assert.assertEquals(expectedPath,actualPath);
+    }
+
+    @Test
+    public void checkNullForNonexsistentKeys(){
+        //given
+        //when
+        String actualPath = imageUtilities.getCardImageTreeMap().get("SPADESKINGS");
+        //then
+        Assert.assertEquals(null,actualPath);
+    }
+
+    /*
+   @Test
+    public void createImageFromKey(){
+        //given
+       String keyString = "CLUBSTHREE";
+       Double expectdHeight = 240.0;
+
+       //when
+       imageView = new ImageView(imageUtilities.cardImageCreator(keyString));
+       Image actual = imageView.getImage();
+       Double actualHeight = actual.getRequestedHeight();
+       //then
+       Assert.assertEquals(expectdHeight, actualHeight);
+   }
+   */
 
 }
